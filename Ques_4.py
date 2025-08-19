f551f11a0ea83c659893763b1c662c35dc41bd02
BHARAT KUNTAL <bharat.kuntal_bca24@gla.ac.in>
2025-08-19 20:21:53 +0530

    Update Ques_4.py

--- Ques_4.py
+++ Ques_4.py
@@ -1,8 +1,8 @@
-# WAP to check if the number is divisible by 7 or if the last digit is 5.
+#     4. WAP to check if the number is divisible by 7 or if the last digit is 5.
 
-num = int(input("Enter a number: "))
 
-if num % 7 == 0 and num % 10 == 5:
-    print(f"{num} is divisible by 7 and ends with 5")
+num = int(input("enter a number : "))
+if(num%7==0 and num%10==5):
+    print("yes divisible by 7 and last digit is 5 ")
 else:
-    print(f"{num} number is not divisible by 7 ")
+    print("NO!!! not divisible by 7 and last digit is 5") 
