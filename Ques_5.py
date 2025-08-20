f60ad52a8189e326ec4f9e365e819bcb9bb82bfb
BHARAT KUNTAL <bharat.kuntal_bca24@gla.ac.in>
2025-08-20 22:16:37 +0530

    Update Ques_5.py

--- Ques_5.py
+++ Ques_5.py
@@ -4,7 +4,8 @@
 A = int(input("Enter an integer: "))
 
 if (A % 5 == 0 and A % 11 == 0):
-    print("YES, it is both divisible by 5 and 11.")
+    print("it is both divisible by 5 and 11.")
 else:
-    print("NO, it is not divisible by 5 and 11.")
+    print("it is not divisible by 5 and 11.")
+
 
