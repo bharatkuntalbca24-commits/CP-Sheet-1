8f2df7e24a67e278e0a33d8a710055be274f5c45
BHARAT KUNTAL <bharat.kuntal_bca24@gla.ac.in>
2025-08-20 22:16:04 +0530

    Update Ques_4.py

--- Ques_4.py
+++ Ques_4.py
@@ -3,6 +3,7 @@
 
 num = int(input("enter a number : "))
 if(num%7==0 and num%10==5):
-    print("yes divisible by 7 and last digit is 5 ")
+    print("divisible by 7 and last digit is 5 ")
 else:
-    print("NO!!! not divisible by 7 and last digit is 5") 
+    print(" not divisible by 7 and last digit is 5") 
+
