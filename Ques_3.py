1732b4a8249b432841ef33463aea1e76f49cd3aa
BHARAT KUNTAL <bharat.kuntal_bca24@gla.ac.in>
2025-08-20 22:15:40 +0530

    Update Ques_3.py

--- Ques_3.py
+++ Ques_3.py
@@ -2,6 +2,7 @@
 
 num = int(input("enter a number : "))
 if(num%3==0 and num%10==4):
-    print("yes divisible by 3 and last digit is 4 ")
+    print("divisible by 3 and last digit is 4 ")
 else:
-    print("NO!!! not divisible by 3 and last digit is 4") 
+    print(" not divisible by 3 and last digit is 4") 
+
