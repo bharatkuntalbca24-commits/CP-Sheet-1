66c77e6db3896a138366fb551a090be60b6e45ad
BHARAT KUNTAL <bharat.kuntal_bca24@gla.ac.in>
2025-08-20 22:15:05 +0530

    Update Ques_2.py

--- Ques_2.py
+++ Ques_2.py
@@ -5,6 +5,7 @@ num = int(input("Enter a number: "))
 last_digit = num % 10
 
 if last_digit == 4:
-    print("Yes, the last digit is 4.")
+    print(" last digit is 4.")
 else:
-    print("No, the last digit is not 4.")
+
+    print(" last digit is not 4.")
